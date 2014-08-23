af6806918051904ebf421164f618ee3ecd5c4c0d
somkiat <somkiat.p@gmail.com>
2014-08-23 12:03:20 +0700

    Add initial sourcecode

---
+++ src/grade.php
@@ -0,0 +1,21 @@
+<?php
+
+class Grade {
+
+	public function tad($score) {
+		if($score == 100)
+			return "F";
+		if($score >= 80)
+			return "A";
+		else if($score >= 70)
+			return "B";
+		else if($score >= 60)
+			return "C";
+		else if($score >= 50)
+			return "D";
+		return "F";
+	}
+
+}
+
+?>
---
+++ test/grade_test.php
@@ -0,0 +1,36 @@
+<?php
+
+class GradeUnitTest extends PHPUnit_Framework_TestCase {
+
+	function test_score_80_should_get_grade_A() {
+		$grade = new Grade();
+		$this->assertEquals("A", $grade->tad(80));
+	}
+
+	function test_score_100_should_get_grade_F() {
+		$grade = new Grade();
+		$this->assertEquals("F", $grade->tad(100));
+	}
+	
+	function test_score_70_should_get_grade_B() {
+		$grade = new Grade();
+		$this->assertEquals("B", $grade->tad(70));
+	}
+
+	function test_score_60_should_get_grade_C() {
+		$grade = new Grade();
+		$this->assertEquals("C", $grade->tad(60));
+	}
+
+	function test_score_50_should_get_grade_D() {
+		$grade = new Grade();
+		$this->assertEquals("D", $grade->tad(50));
+	}
+
+	function test_score_49_should_get_grade_F() {
+		$grade = new Grade();
+		$this->assertEquals("F", $grade->tad(49));
+	}
+
+}
+?>
